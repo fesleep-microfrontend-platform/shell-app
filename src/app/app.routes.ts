f8efb301300f4db9e8194787d19eb958b134fc99
Felipe Gomes <felipegomes@Felipes-MacBook-Air.local>
2026-03-01 17:27:06 -0300

    project creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'contact',
+    loadComponent: () => 
+        loadRemoteModule('contact-Home', './ContactHome')
+            .then(m => m.ContactHome)
+  },
+];
